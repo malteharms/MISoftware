176cb2dd8896ce95e0f8319620776561bb751a25
malteharms <malte.harms.1@stud.hs-emden-leer.de>
2023-10-22 17:37:40 +0200

    file structure for app page

--- app/src/main/java/de/malteharms/misoftware/ui/components/screens/FunctionScreen.kt
+++ app/src/main/java/de/malteharms/misoftware/ui/components/screens/FunctionScreen.kt
@@ -4,6 +4,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Build
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -12,6 +15,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.dp
+import de.malteharms.misoftware.ui.components.screens.costs.CostsPreview
+import de.malteharms.misoftware.ui.components.wrapper.AppWrapper
 import de.malteharms.misoftware.ui.theme.MISoftwareTheme
 
 @Composable
@@ -26,12 +31,35 @@ fun FunctionScreen(navController: NavController) {
                     .fillMaxSize()
                     .padding(15.dp),
                 horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
+                verticalArrangement = Arrangement.Top
             ) {
-                Text(
-                    "Function Screen",
-                    style = MaterialTheme.typography.titleLarge,
-                    modifier = Modifier.padding(vertical = 20.dp)
+                AppWrapper(
+                    preview = { Text("Kalender") },
+                    title = "Kalender",
+                    icon = Icons.Default.Build,
+                    magic = { Text(text = "magic")},
+                    navController = navController
+                )
+                AppWrapper(
+                    preview = { CostsPreview(navController = navController) },
+                    title = "Costs",
+                    icon = Icons.Default.Build,
+                    magic = { Text(text = "magic")},
+                    navController = navController
+                )
+                AppWrapper(
+                    preview = { Text("Shopping") },
+                    title = "Shopping",
+                    icon = Icons.Default.ShoppingCart,
+                    magic = { Text(text = "magic")},
+                    navController = navController
+                )
+                AppWrapper(
+                    preview = { Text("ToDo") },
+                    title = "ToDo",
+                    icon = Icons.Default.Build,
+                    magic = { Text(text = "magic")},
+                    navController = navController
                 )
             }
         }
